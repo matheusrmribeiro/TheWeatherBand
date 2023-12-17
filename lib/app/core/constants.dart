077f20c9659ca5199017423d697f46758c38af73
Matheus Ribeiro <matheusrmribeiro@gmail.com>
2023-12-17 17:58:12 -0300

    Adjustment.

--- lib/app/core/constants.dart
+++ lib/app/core/constants.dart
@@ -1,6 +1,6 @@
 const String kSERVER_BASE = "https://api.openweathermap.org/data/2.5/";
 const String kSERVER_BASE_GEO = "https://api.openweathermap.org/geo/1.0/";
-const String kAPI_KEY = "44bf01765f57ef51796f8777d510f68e";
+const String kAPI_KEY = ""; /// Get your key on https://openweathermap.org/"
 const String kUNITS_MEASUREMENT = "metric";
 
 const int FORECAST_TRACKING_DAYS = 6;
